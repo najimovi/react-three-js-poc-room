5f8a04c39fb009bbb1ea829068664b85d2b1b073
Pablo Najimovich <pablo.najimovich@gmail.com>
2021-10-13 15:04:00 -0300

    Updates

---
+++ precache-manifest.de7c58134a355295f66f4d1b0e70ad40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cdbc39ce2eb09ecd8bac0b220e47456",
+    "url": "/react-three-fiber-demo/index.html"
+  },
+  {
+    "revision": "a79fd9f4917ea2e86983",
+    "url": "/react-three-fiber-demo/static/css/main.80583a33.chunk.css"
+  },
+  {
+    "revision": "ffe053d6cfda5e1e4cb1",
+    "url": "/react-three-fiber-demo/static/js/2.e2f4a503.chunk.js"
+  },
+  {
+    "revision": "a79fd9f4917ea2e86983",
+    "url": "/react-three-fiber-demo/static/js/main.3b7b5f66.chunk.js"
+  },
+  {
+    "revision": "c4c123700f1272ed85f7",
+    "url": "/react-three-fiber-demo/static/js/runtime~main.1344da34.js"
+  }
+]);
